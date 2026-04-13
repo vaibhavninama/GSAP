39e5e82c200a58466ca56203e7107febbb359815
Vaibhavninama <vaibhavninama172@gmail.com>
2026-04-13 11:46:58 +0530

    Add Nav animation

--- script.js
+++ script.js
@@ -1,4 +1,47 @@
 
+let ti =gsap.timeline()
+let st =gsap.timeline()
+
+st.from('nav h1',{
+    opacity:0,
+    y:-100,
+    duration:1.5,
+    ease:'power2.inOut'
+})
+
+st.from('nav i',{
+    opacity:0,
+    
+    duration:0.5,
+    ease:'power2.inOut'
+})
+
+
+ti.to('.full',{
+     right:'2vw',
+    
+})
+ti.from('.full h4',{
+  opacity:0,
+  x:100,
+  stagger:0.3,
+
+
+})
+ti.from('.full i',{
+  
+    opacity:0,
+
+})
+
+ti.pause()
+
+document.querySelector('.full i').addEventListener('click',()=>{
+  ti.reverse()
+})
+document.querySelector('nav i').addEventListener('click',()=>{
+  ti.play()
+})
 
 gsap.to('.page22 h1',{
     transform:'translateX(-45%)',
